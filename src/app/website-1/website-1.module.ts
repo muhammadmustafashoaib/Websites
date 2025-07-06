4e77fc077e6e3be96398332deb8f02a4e3599932
Mustafa Shoaib <152904440+muhammadmustafashoaib@users.noreply.github.com>
2025-07-06 17:16:52 +0500

    Plumbing website 1

---
+++ src/app/website-1/website-1.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ]
+})
+export class Website1Module { }
