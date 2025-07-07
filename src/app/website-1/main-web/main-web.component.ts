56d805046c491e956cceff3340f6a6828957666a
Mustafa Shoaib <152904440+muhammadmustafashoaib@users.noreply.github.com>
2025-07-07 14:23:18 +0500

    website 1 created

---
+++ src/app/website-1/main-web/main-web.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-web',
+  templateUrl: './main-web.component.html',
+  styleUrls: ['./main-web.component.css']
+})
+export class MainWebComponent {
+
+}
--- src/app/website-1/website-1.module.ts
+++ src/app/website-1/website-1.module.ts
@@ -1,12 +1,40 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
 
+import { MainWebComponent } from './main-web/main-web.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
+import { ContactComponent } from './contact/contact.component';
+import { HeroComponent } from './hero/hero.component';
 
+const routes: Routes = [
+  {
+    path: '',
+    component: MainWebComponent,
+  
+  }
+];
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    MainWebComponent,
+    LandingPageComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    ServicesComponent,
+    TestimonialsComponent,
+    ContactComponent,
+    HeroComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)  // 👈 this is key
   ]
 })
 export class Website1Module { }
