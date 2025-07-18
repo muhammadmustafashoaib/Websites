fa4eade15b9c134a04374f8c1d749b8dba8c4d55
Mustafa Shoaib <152904440+muhammadmustafashoaib@users.noreply.github.com>
2025-07-18 14:34:11 +0500

    Added Routing and information in components

--- Website Sample 2/website-2/src/app/app-routing.module.ts
+++ Website Sample 2/website-2/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { FaqComponent } from './faq/faq.component';
+import { HomeComponent } from './home/home.component';
+import { ServicesComponent } from './services/services.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'faq', component: FaqComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
