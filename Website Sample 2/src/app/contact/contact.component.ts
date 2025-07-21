8376cd274237d0ab758deb7f573f87cacc5c6265
Mustafa Shoaib <152904440+muhammadmustafashoaib@users.noreply.github.com>
2025-07-21 11:33:23 +0500

    updated home and services

--- Website Sample 2/src/app/contact/contact.component.ts
+++ Website Sample 2/src/app/contact/contact.component.ts
@@ -60,4 +60,36 @@ export class ContactComponent {
         this.formData.status = '❌ Network error. Please try again later.';
       });
   }
+   reviews = [
+    {
+      name: 'Ali R.',
+      message: 'Excellent plumbing service. Very professional and on time!',
+      rating: 5
+    },
+    {
+      name: 'Fatima S.',
+      message: 'Quick response and solved the leakage in minutes. Recommended!',
+      rating: 4
+    },
+    {
+      name: 'Zain A.',
+      message: 'Courteous staff and clean installation. Would hire again.',
+      rating: 5
+    },
+    {
+      name: 'Sarah M.',
+      message: 'Good service, but arrived a little late. Overall okay.',
+      rating: 3
+    },
+    {
+      name: 'Umar N.',
+      message: 'Solved a long-standing drainage issue. Great work!',
+      rating: 5
+    },
+    {
+      name: 'Hira K.',
+      message: 'Easy to book, came with all tools. Satisfied with service.',
+      rating: 4
+    }
+  ];
 }
--- Website Sample 2/src/app/home/home.component.ts
+++ Website Sample 2/src/app/home/home.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+ reviews = [
+    {
+      name: 'Ali R.',
+      message: 'Excellent plumbing service. Very professional and on time!',
+      rating: 5
+    },
+    {
+      name: 'Fatima S.',
+      message: 'Quick response and solved the leakage in minutes. Recommended!',
+      rating: 4
+    },
+    {
+      name: 'Zain A.',
+      message: 'Courteous staff and clean installation. Would hire again.',
+      rating: 5
+    },
+    {
+      name: 'Sarah M.',
+      message: 'Good service, but arrived a little late. Overall okay.',
+      rating: 3
+    },
+    {
+      name: 'Umar N.',
+      message: 'Solved a long-standing drainage issue. Great work!',
+      rating: 5
+    },
+    {
+      name: 'Hira K.',
+      message: 'Easy to book, came with all tools. Satisfied with service.',
+      rating: 4
+    }
+  ];
 }
