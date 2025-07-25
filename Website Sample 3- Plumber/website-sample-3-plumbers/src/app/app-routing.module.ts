e81f3b5fa40c44219b080ba6bce1da0cb3e8b6cd
Mustafa Shoaib <152904440+muhammadmustafashoaib@users.noreply.github.com>
2025-07-25 10:41:31 +0500

    fixed routing

--- Website Sample 3- Plumber/website-sample-3-plumbers/src/app/app-routing.module.ts
+++ Website Sample 3- Plumber/website-sample-3-plumbers/src/app/app-routing.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { HeroComponent } from './hero/hero.component';
 
 const routes: Routes = [
-  {path:'about', component:AboutComponent}
+  {path:'', component:HeroComponent},
+  { path: 'services', component: ServicesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactFormComponent }
 ];
 
 @NgModule({
